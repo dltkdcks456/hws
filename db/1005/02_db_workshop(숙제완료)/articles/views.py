63e5a162a40d1c5a644c6f13b7184b76da619a4b
dltkdcks456 <sangchan0825@gmail.com>
2022-10-05 22:27:11 +0900

    10.05

--- db/1005/02_db_workshop(숙제완료)/articles/views.py
+++ db/1005/02_db_workshop(숙제완료)/articles/views.py
@@ -1,5 +1,5 @@
-from .models import Article
-from .forms import ArticleForm
+from .models import Article, Comment
+from .forms import ArticleForm, CommentForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods, require_POST, require_safe
@@ -34,8 +34,12 @@ def create(request):
 @require_safe
 def detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
+    comment_form = CommentForm()
+    comments = article.comment_set.all()
     context = {
         'article': article,
+        'comment_form':comment_form,
+        'comments': comments,
     }
     return render(request, 'articles/detail.html', context)
 
@@ -67,3 +71,17 @@ def update(request, pk):
         'form': form,
     }
     return render(request, 'articles/update.html', context)
+
+def comments_create(request, pk):
+    article = Article.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.article = article
+        comment.save()
+    return redirect('articles:detail', article.pk)
+
+def comments_delete(request, article_pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect('articles:detail', article_pk)
